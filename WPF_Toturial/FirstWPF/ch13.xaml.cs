ad4cd63b689b11c1f8065cb2ba9f7e490b4c9d55
jihun <jihun315016@gmail.com>
2023-10-28 00:31:29 +0900

    ch13, ch14

---
+++ WPF_Toturial/FirstWPF/ch13.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FirstApp
+{
+    /// <summary>
+    /// ch13.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class ch13 : Window
+    {
+        public ch13()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                txtPath.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void btnRead_Click(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists(txtPath.Text))
+            {
+                using (StreamReader sr = new StreamReader(txtPath.Text))
+                {
+                    // ReadToEnd()
+                    // 파일에 있는 내용을 한 번에 읽어와서 모두 반환해주는 함수
+                    richTxt.Document.Blocks.Clear();
+                    richTxt.AppendText(sr.ReadToEnd());
+                }
+            }
+            else
+            {
+                MessageBox.Show("파일이 없습니다.", "에러", MessageBoxButton.OK);
+            }
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            richTxt.Document.Blocks.Clear();
+        }
+    }
+}
---
+++ WPF_Toturial/FirstWPF/ch14.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FirstApp
+{
+    /// <summary>
+    /// ch14.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class ch14 : Window
+    {
+        public ch14()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text File(*.txt)|*.txt|XML File(*.xml)|*.xml|JSON File(*.json)|*.json";
+            
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                using (StreamWriter sw = new StreamWriter(saveFileDialog.FileName))
+                {
+                    // richTxt 컨텐트 시작부터 끝까지 범위 지정
+                    TextRange textRange = new TextRange(richTxt.Document.ContentStart, richTxt.Document.ContentEnd);
+                    sw.WriteLine(textRange.Text);
+                    MessageBox.Show("파일이 저장되었습니다.");
+                }
+            }
+        }
+    }
+}
