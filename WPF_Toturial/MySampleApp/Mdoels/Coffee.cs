b77d12e5a0f4c1d255b5290148760a41916d8fe3
jihun <jihun315016@gmail.com>
2023-11-11 17:24:28 +0900

    MySampleApp

---
+++ WPF_Toturial/MySampleApp/Mdoels/Coffee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySampleApp.Mdoels
+{
+    public class Coffee
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public bool DecafOption { get; set; }
+    }
+}
