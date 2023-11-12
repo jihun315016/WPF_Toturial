cccae5dd1efb4c6e2d57a149ecb5abfdc186c6b6
jihun <jihun315016@gmail.com>
2023-11-12 16:22:02 +0900

    MySampleApp 파일 구성

---
+++ WPF_Toturial/MySampleApp/Mdoels/CoffeeDAO.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace MySampleApp.Mdoels
+{
+    public class CoffeeDAO : IDisposable, ICoffeeDAO
+    {
+        SqlConnection conn;
+
+        public CoffeeDAO()
+        {
+            string connStr = "";
+            conn = new SqlConnection(connStr);
+            conn.Open();
+        }
+
+        public void Dispose()
+        {
+            if (conn != null && conn.State == ConnectionState.Open) 
+            {
+                conn.Close();
+            }
+        }
+
+        public bool Delete(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public IEnumerable<Coffee> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save(Coffee coffee)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WPF_Toturial/MySampleApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using MySampleApp.Mdoels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MySampleApp.ViewModels
+{
+    public class MainViewModel
+    {
+        public ICommand SaveCommand { get; set; }
+        public ICommand DeleteCommand { get; set; }
+
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public bool DecafOption { get; set; }
+
+        private ICoffeeDAO _coffeeDAO;
+        private ObservableCollection<Coffee> _coffeeList;
+
+
+        public MainViewModel(ICoffeeDAO coffeeDAO)
+        {
+            _coffeeDAO = coffeeDAO;
+
+            
+        }
+    }
+}
