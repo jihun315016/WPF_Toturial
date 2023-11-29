7300308fe7d6df8c386de3b87b9b5d6d724b7834
jihun <jihun315016@gmail.com>
2023-11-29 22:59:04 +0900

    binding sample

---
+++ WPF_Toturial/SampleSource/Bind/Coffee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleSource.Bind
+{
+    public class Coffee
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Img { get; set; }
+    }
+}
--- WPF_Toturial/SampleSource/Bind/exBinding1.xaml.cs
+++ WPF_Toturial/SampleSource/Bind/exBinding1.xaml.cs
@@ -17,9 +17,9 @@ namespace SampleSource.Bind
     /// <summary>
     /// exBinding1.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class exBinding : Window
+    public partial class exBinding1 : Window
     {
-        public exBinding()
+        public exBinding1()
         {
             InitializeComponent();
 
