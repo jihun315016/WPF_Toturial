71004a07c1dbbc6d3f7e2514e55e2ffa3958a1eb
jihun <jihun315016@gmail.com>
2023-11-30 00:05:01 +0900

    바인딩 샘플 소스 작성 중

---
+++ WPF_Toturial/SampleSource/Bind/Beverage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleSource.Bind
+{
+    public class Beverage
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Img { get; set; }
+        public BeverageCategory Category { get; set; }
+    }
+
+
+    public class Coffee : Beverage 
+    {
+        // Coffee 고유의 속성이 있다면 작성
+
+        public Coffee() 
+        { 
+            Category = BeverageCategory.Coffee;
+            Img = @"/Assets/coffee.png";
+            Img = @"C:\Users\jihun\coding\WPF_Toturial\WPF_Toturial\SampleSource\Assets\coffee.png";
+        }
+    }
+    
+
+    public class Tea : Beverage 
+    {
+        // Tea 고유의 속성이 있다면 작성
+
+        public Tea()
+        {
+            Category = BeverageCategory.Tea;
+            Img = @"/Assets/tea.png";
+        }
+    }
+
+
+    public enum BeverageCategory
+    {
+        Coffee,
+        Tea
+    }
+}
--- WPF_Toturial/SampleSource/Bind/exBinding2.xaml.cs
+++ WPF_Toturial/SampleSource/Bind/exBinding2.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +20,37 @@ namespace SampleSource.Bind
     /// </summary>
     public partial class exBinding2 : Window
     {
+        List<Beverage> beverageList;
+
         public exBinding2()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine(System.Environment.CurrentDirectory);
+            string imagePath = "pack://application:,,,/Assets/tes.png";
+            if (System.IO.File.Exists(imagePath))
+            {
+                MessageBox.Show("!!");
+            }
+            else
+            {
+                MessageBox.Show("??");
+            }
+            beverageList = new List<Beverage>()
+            {
+                new Coffee() {Name = "아메리카노", Price = 1500},
+                new Coffee() {Name = "카페라떼", Price = 2500},
+                new Coffee() {Name = "콜드브루", Price = 2500},
+                new Coffee() {Name = "카페모카", Price = 3500},
+                new Tea() {Name = "아이스티", Price = 1500},
+                new Tea() {Name = "캐모마일", Price = 2000},
+                new Tea() {Name = "페퍼민트", Price = 2000}
+            };
+
+            lst.ItemsSource = beverageList.FindAll(b => b.Category == BeverageCategory.Coffee);
         }
     }
 }
