a1a326188838da3a21744b16d0a0d425f532b0e1
jihun <jihun315016@gmail.com>
2023-12-07 20:54:19 +0900

    DesignPattern 프로젝트 복구

---
+++ WPF_Toturial/WPF.DesignPattern/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using WPF.DesignPattern.MVVM.Models;
+
+namespace WPF.DesignPattern
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var personRepository = new PersonRepository();
+
+            var mainView = new MVVM.Views.MainView()
+            {
+                // 데이터 바인딩을 위한 작업
+                // View(xaml)에서 MainViewModel의 속성들을 바인딩할 수 있게 된다.
+                // DataContext -> UI에 바인딩 될 데이터 소스(데이터가 오는 곳)
+                DataContext = new MVVM.ViewModels.MainViewModel(personRepository)
+            };
+            mainView.Show();
+        }
+    }
+}
